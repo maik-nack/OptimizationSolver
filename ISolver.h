0504ac70004e29ee7a937cbc00442622464abf58
Mikhail Nakhatovich <mish_1998@mail.ru>
2019-06-01 20:28:48 +0300

    Updated existing interfaces

--- ISolver.h
+++ ISolver.h
@@ -1,15 +1,9 @@
 #ifndef ISOLVER_H
 #define ISOLVER_H
 
-// #include "error.h"
-// #include <QVector>
 #include <QUrl>
-#include "error.h"
+#include "IProblem.h"
 #include "SHARED_EXPORT.h"
-
-class IProblem;
-class ICompact;
-class IVector;
 
 class SHARED_EXPORT ISolver
 {
@@ -22,15 +16,12 @@ public:
 
     virtual int getId() const = 0;
 
-    // static ISolver* createSolver();
-
     virtual int setParams(IVector const* params) = 0;
     virtual int setParams(QString& str) = 0;
     virtual int setProblem(IProblem *ptr) = 0;
     virtual int solve() = 0;
     virtual int getSolution(IVector* &vec)const = 0;
     virtual int getQml(QUrl& qml) const = 0;
-    // virtual ~ISolver() = default;
 
 protected:
     virtual ~ISolver() = default;
@@ -40,7 +31,7 @@ protected:
 private:
     /*non default copyable*/
     ISolver(const ISolver& other) = delete;
-    void operator=( const ISolver& other) = delete;
+    void operator=(const ISolver& other) = delete;
 };
 
 #endif // ISOLVER_H
